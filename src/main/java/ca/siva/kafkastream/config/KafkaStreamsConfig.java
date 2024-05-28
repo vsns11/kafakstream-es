30766e593f616b8e9e6e79f593a0515cda483ffa
sivanagasaivemula <vemula.sivanagasai@gmail.com>
2024-05-28 13:58:28 -0400

    Added joinwindow

--- src/main/java/ca/siva/kafkastream/config/KafkaStreamsConfig.java
+++ src/main/java/ca/siva/kafkastream/config/KafkaStreamsConfig.java
@@ -2,7 +2,6 @@ package ca.siva.kafkastream.config;
 
 import ca.siva.kafkastream.GenericRecordUtil;
 import ca.siva.kafkastream.model.AggregatedData;
-import ca.siva.kafkastream.processor.SuppressWindowProcessorSupplier;
 import ca.siva.kafkastream.service.ElasticsearchService;
 import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig;
 import io.confluent.kafka.streams.serdes.avro.GenericAvroSerde;
@@ -10,18 +9,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
-import org.apache.kafka.common.utils.Bytes;
-import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.kstream.*;
-import org.apache.kafka.streams.processor.Cancellable;
-import org.apache.kafka.streams.processor.PunctuationType;
-import org.apache.kafka.streams.processor.Punctuator;
-import org.apache.kafka.streams.processor.api.Processor;
-import org.apache.kafka.streams.processor.api.ProcessorContext;
-import org.apache.kafka.streams.processor.api.ProcessorSupplier;
-import org.apache.kafka.streams.processor.api.Record;
-import org.apache.kafka.streams.state.*;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -30,9 +19,8 @@ import org.springframework.kafka.annotation.EnableKafkaStreams;
 import org.springframework.kafka.support.serializer.JsonSerde;
 
 import java.time.Duration;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.Map;
-import java.util.Set;
 
 import static ca.siva.kafkastream.GenericRecordUtil.convertValue;
 import static ca.siva.kafkastream.GenericRecordUtil.findNestedValue;
@@ -43,7 +31,6 @@ import static ca.siva.kafkastream.GenericRecordUtil.findNestedValue;
 public class KafkaStreamsConfig {
 
     private final ElasticsearchService elasticsearchService;
-    private final Duration inactivityGap = Duration.ofMinutes(2);
 
     @Value("${kafka.topic.input}")
     private String inputTopic;
@@ -63,92 +50,88 @@ public class KafkaStreamsConfig {
 
     @Bean(name = "customSerdeConfig")
     public Map<String, ?> serdeConfig() {
-        return Map.of(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryUrl, "specific.avro.reader", specificAvroReader);
+        return Map.of(
+                AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryUrl,
+                "specific.avro.reader", specificAvroReader
+        );
     }
 
     @Bean
     public KStream<String, GenericRecord> kStream(StreamsBuilder streamsBuilder, @Qualifier("customSerdeConfig") Map<String, ?> serdeConfig) {
         final Serde<GenericRecord> valueGenericAvroSerde = new GenericAvroSerde();
-        log.info("map: {}", serdeConfig);
         valueGenericAvroSerde.configure(serdeConfig, false);
-        Serde<AggregatedData> aggregatedJsonMessageSerde = new JsonSerde<>(AggregatedData.class);
 
-        // Define the state store
-//        StoreBuilder<SessionStore<String, AggregatedData>> storeBuilder = Stores.sessionStoreBuilder(
-//                Stores.persistentSessionStore("aggregated-window-store", Duration.ofMinutes(5)),
-//                Serdes.String(),
-//                aggregatedJsonMessageSerde
-//        );
-//        streamsBuilder.addStateStore(storeBuilder);
+        final Serde<AggregatedData> aggregatedJsonMessageSerde = new JsonSerde<>(AggregatedData.class);
 
         KStream<String, GenericRecord> stream = streamsBuilder.stream(inputTopic, Consumed.with(Serdes.String(), valueGenericAvroSerde));
 
-        Named named = Named.as("branch");
-        Map<String, KStream<String, GenericRecord>> branches = stream.split(named).branch((key, genericRecord) -> {
-            Object nameFieldValue = convertValue(findNestedValue(genericRecord, "name"));
-            Object bpiIdFieldValue = convertValue(findNestedValue(genericRecord, "bpiId"));
+        // Split the stream into initial and other messages
+        Map<String, KStream<String, GenericRecord>> branches = stream.split(Named.as("branch"))
+                .branch((key, genericRecord) -> {
+                    Object nameFieldValue = convertValue(findNestedValue(genericRecord, "name"));
+                    Object bpiIdFieldValue = convertValue(findNestedValue(genericRecord, "bpiId"));
 
-            log.info("name read: {}, type: {}", nameFieldValue, nameFieldValue.getClass().getName());
-            log.info("bpiId read: {}, type: {}", bpiIdFieldValue, bpiIdFieldValue.getClass().getName());
-
-            if (nameFieldValue instanceof String name && bpiIdFieldValue instanceof String) {
-                log.info("First message Data: {}, name: {}, bpiId: {}", genericRecord, name, bpiIdFieldValue);
-                return name.equalsIgnoreCase(filterValue);
-            }
-            return false;
-        }, Branched.as("firstMessage")).defaultBranch(Branched.as("otherMessages"));
+                    if (nameFieldValue instanceof String name && bpiIdFieldValue instanceof String) {
+                        log.info("Received event with name: {}, bpiId: {}", nameFieldValue, bpiIdFieldValue);
+                        return name.equalsIgnoreCase(filterValue);
+                    }
+                    return false;
+                }, Branched.as("firstMessage"))
+                .defaultBranch(Branched.as("otherMessages"));
 
         KStream<String, GenericRecord> firstMessageStream = branches.get("branchfirstMessage");
         KStream<String, GenericRecord> otherMessagesStream = branches.get("branchotherMessages");
 
         if (firstMessageStream == null || otherMessagesStream == null) {
-            log.error("branch keys :{}", branches.keySet());
+            log.error("branch keys: {}", branches.keySet());
             throw new IllegalStateException("Streams not properly initialized");
         }
 
-        KTable<String, GenericRecord> firstMessageTable = firstMessageStream.selectKey((String key, GenericRecord value) -> {
-            String newKey = (String) convertValue(findNestedValue(value, "bpiId"));
-            log.info("selectKey for firstMessageTable: key = {}, newKey = {}", key, newKey);
-            return newKey;
-        }).toTable(Materialized.with(Serdes.String(), valueGenericAvroSerde));
+        firstMessageStream.peek((key, value) -> {
+            AggregatedData aggregatedData = AggregatedData.builder()
+                    .id((String) convertValue(findNestedValue(value, "bpiId")))
+                    .relatedEvents(new ArrayList<>())
+                    .build();
+            aggregatedData.add(GenericRecordUtil.convertGenericRecordToMap(value));
+            log.info("Ingesting initial event to Elasticsearch: key = {}, value = {}", key, aggregatedData);
 
-        KStream<String, GenericRecord> rekeyedOtherMessagesStream = otherMessagesStream.selectKey((String key, GenericRecord value) -> {
-            String newKey = (String) convertValue(findNestedValue(value, "bpiId"));
-            log.info("selectKey for otherMessagesStream: key = {}, newKey = {}", key, newKey);
-            return newKey;
+            elasticsearchService.ingestToEs(aggregatedData);
         });
 
-        KStream<String, Map<String, Object>> joinedStream = rekeyedOtherMessagesStream.join(firstMessageTable, (otherMessage, firstMessage) -> {
-            log.info("Processing for joinedStream: otherMessage = {}, firstMessage = {}", otherMessage, firstMessage);
-            Map<String, Object> combinedMap = new HashMap<>();
-            combinedMap.put("initialEvent", GenericRecordUtil.convertGenericRecordToMap(firstMessage));
-            combinedMap.put("receivedEvent", GenericRecordUtil.convertGenericRecordToMap(otherMessage));
-            return combinedMap;
+        // Create a KTable from the firstMessageStream
+        KStream<String, GenericRecord> rekeyedFirstMessagesStream = firstMessageStream
+                .selectKey((key, value) -> (String) convertValue(findNestedValue(value, "bpiId")));
+
+        // Rekey other messages by bpiId
+        KStream<String, GenericRecord> rekeyedOtherMessagesStream = otherMessagesStream
+                .selectKey((key, value) -> (String) convertValue(findNestedValue(value, "bpiId")));
+
+        // Create a windowed join between the streams
+        Duration joinWindowDuration = Duration.ofMinutes(40);
+        KStream<String, AggregatedData> joinedStream = rekeyedOtherMessagesStream
+                .join(
+                        rekeyedFirstMessagesStream.selectKey((key, value) -> (String) convertValue(findNestedValue(value, "bpiId"))),
+                        (otherMessage, firstMessage) -> {
+                            log.info("Joining otherMessage: {}, with firstMessage: {}", otherMessage, firstMessage);
+                            AggregatedData aggregatedData = AggregatedData.builder()
+                                    .id((String) convertValue(findNestedValue(firstMessage, "bpiId")))
+                                    .latestUpdateTimestamp((String) convertValue(findNestedValue(otherMessage, "timestamp")))
+                                    .relatedEvents(new ArrayList<>())
+                                    .build();
+                            aggregatedData.add(GenericRecordUtil.convertGenericRecordToMap(otherMessage));
+                            return aggregatedData;
+                        },
+                        JoinWindows.ofTimeDifferenceAndGrace(joinWindowDuration, Duration.ofMinutes(5)),
+                        StreamJoined.with(Serdes.String(), valueGenericAvroSerde, valueGenericAvroSerde)
+                );
+
+
+        // Process the joined stream and push the subsequent events to Elasticsearch
+        joinedStream.peek((key, value) -> {
+            log.info("Ingesting subsequent event to Elasticsearch: key = {}, value = {}", key, value);
+            elasticsearchService.ingestToEs(value);
         });
 
-        KTable<Windowed<String>, AggregatedData> aggregatedTable = joinedStream
-                .groupByKey(Grouped.with(Serdes.String(), new JsonSerde<>()))
-                .windowedBy(SessionWindows.ofInactivityGapAndGrace(Duration.ofSeconds(30), Duration.ofSeconds(10)))
-                .aggregate(AggregatedData::new, (key, value, aggregate) -> {
-            log.info("Aggregating key: {}, value: {}", key, value);
-            if (!aggregate.isStartEventAdded()) {
-                @SuppressWarnings("unchecked") Map<String, Object> firstMessageMap = (Map<String, Object>) value.get("initialEvent");
-                aggregate.add(firstMessageMap);
-                aggregate.setStartEventAdded(true);
-            }
-            aggregate.add((Map<String, Object>) value.get("receivedEvent"));
-            return aggregate;
-        }, (aggKey, aggOne, aggTwo) -> {
-            log.info("Merging aggregates: aggOne = {}, aggTwo = {}", aggOne, aggTwo);
-            aggOne.merge(aggTwo);
-            return aggOne;
-        }, Materialized.<String, AggregatedData, SessionStore<Bytes, byte[]>>as("aggregated-window-store")
-                        .withKeySerde(Serdes.String())
-                        .withValueSerde(aggregatedJsonMessageSerde))
-                .suppress(Suppressed.untilWindowCloses(Suppressed.BufferConfig.unbounded()));
-
-        aggregatedTable.toStream()
-                .process(new SuppressWindowProcessorSupplier(elasticsearchService), "aggregated-window-store");
         return stream;
     }
 }
--- src/main/java/ca/siva/kafkastream/processor/IngestPunctuator.java
+++ src/main/java/ca/siva/kafkastream/processor/IngestPunctuator.java
@@ -18,23 +18,44 @@ public class IngestPunctuator implements Punctuator {
     public IngestPunctuator(ProcessorContext<Void, Void> context, ElasticsearchService elasticsearchService) {
         this.context = context;
         this.elasticsearchService = elasticsearchService;
+
     }
 
     @Override
     public void punctuate(long timestamp) {
         log.info("Punctuate called at: {}", timestamp);
         SessionStore<String, AggregatedData> sessionStore = context.getStateStore("aggregated-window-store");
+        KeyValueStore<String, Boolean> processedKeysStore= context.getStateStore("processed-keys-store");
 
         try (KeyValueIterator<Windowed<String>, AggregatedData> iterator = sessionStore.fetch(null, null)) {
             while (iterator.hasNext()) {
                 KeyValue<Windowed<String>, AggregatedData> keyValue = iterator.next();
+                String key = keyValue.key.key();
+
+                // Check if the key has already been processed
+                if (Boolean.TRUE.equals(processedKeysStore.get(key))) {
+                    log.info("Skipping already processed key: {}", key);
+                    continue;
+                }
                 log.info("Ingesting to Elasticsearch: key = {}, value = {}", keyValue.key.key(), keyValue.value);
                 elasticsearchService.ingestToEs(keyValue.value);
+                // Mark the key as processed
+                processedKeysStore.put(key, true);
+
                 log.info("Key getting removed: {}", keyValue.key);
                 sessionStore.remove(keyValue.key);
+                processedKeysStore.delete(key);
+                // Confirm removal
+
+                if (sessionStore.fetch(keyValue.key.key(), keyValue.key.key()).hasNext()) {
+                    log.warn("Key still present after removal: {}", keyValue.key);
+                }
             }
         } catch (Exception e) {
             log.error("Error during punctuate: {}", e.getMessage(), e);
         }
+        log.info("punctuator completed execution: {}", timestamp);
     }
+
+
 }
